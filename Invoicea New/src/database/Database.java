b896cdaefeb1f8e11428066249bc3ac04d93446a
giova <giova@LAPTOP-3RIQ99I3>
2019-11-08 08:04:47 -0500

    Database Comments

--- Invoicea New/src/database/Database.java
+++ Invoicea New/src/database/Database.java
@@ -12,12 +12,13 @@ public class Database {
 	public static Connection connect;
 	public static Statement stmt = null;;
 
-	
+	//Creates the code with the studio_id-year-invoic_id 
 	public static String CreateCode(int studio_id, int year, int invoice_id) {
 		String code = "0" + studio_id + "-" + year + "-00" + invoice_id;
 		return code;
 	}
 	   
+	//Search into the DB if the studio has any invoice, if not, create invoice_id 1
 	public static int HasInvoice(int studio_id) {
 		connect = DBConnect.connectDB();
 		int invoice_id = 0;
@@ -29,17 +30,13 @@ public class Database {
 			while(rs.next()) {
 				invoice_id++;
 			}
-			
 		} catch (SQLException e) {
 			System.out.print(e);
 		} 
-//comment
-		
-		       
-		return invoice_id;
-		
+		return invoice_id;	
 	}
 
+	//If the studio has no invoice, create a new one starting from studio_id 1
 	public static void CreateNewInvoice(int studio_id, int iYear) {
 		connect = DBConnect.connectDB();
 		String cmd = "INSERT INTO invoice(code, studio_id, invoice_id, year) VALUES(?,?,?, ?)";
@@ -55,13 +52,11 @@ public class Database {
 		} catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-		
-		
 	}
 
+	//If the studio has an invoice, create a new one with invoice_id+1
 	public static void CreateInvoice(int studio_id, int invoice_id, int iYear) {
 		connect = DBConnect.connectDB();
-		
 		String cmd = "INSERT INTO invoices(code, studio_id, invoice_id, year) VALUES(?, ?,?, ?)";
 		int inv = invoice_id+1;
 		String code = CreateCode(studio_id, iYear, inv);
@@ -76,15 +71,11 @@ public class Database {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-		
 	}
 	
-	
+	//Insert a new material into the table
 	public static void CreateMaterial(String name, String cost){
 		connect = DBConnect.connectDB();
-		
-		
-		
 		String cmd = "INSERT INTO material(name, cost) VALUES(?, ?)";
 		try (Connection conn = connect;
                 PreparedStatement pstmt = conn.prepareStatement(cmd)) {
@@ -96,4 +87,6 @@ public class Database {
             System.out.println(e.getMessage());
         }
 	}
+	
+	
 }
